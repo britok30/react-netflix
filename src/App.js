a567e142951138798e15279dc955da83b6cc552c
Kelvin Brito <britok30@gmail.com>
2020-10-18 23:26:31 -0400

    setup jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import jumboData from './fixtures/jumbo.json';
 import Jumbotron from './components/jumbotron';
 
 function App() {
-    return <Jumbotron />;
+    return (
+        <Jumbotron.Container>
+            {jumboData.map((item) => (
+                <Jumbotron key={item.id} direction={item.direction}>
+                    <p>{item.title}</p>
+                    <p>{item.subTitle}</p>
+                    <p>{item.image}</p>
+                    <p>{item.alt}</p>
+                </Jumbotron>
+            ))}
+        </Jumbotron.Container>
+    );
 }
 
 export default App;
--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -2,4 +2,18 @@ import styled from 'styled-components/macro';
 
 export const Inner = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1100px;
+    flex-direction: ${({ direction }) => direction};
+    margin: auto;
+    width: 100%;
+
+    @media (max-width: 1000px) {
+        flexr-direction: column;
+    }
 `;
+
+export const Container = styled.div`
+
+`
